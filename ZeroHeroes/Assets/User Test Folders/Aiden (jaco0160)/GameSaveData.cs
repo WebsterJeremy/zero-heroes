1f220887f8227fe32cc7ce0689a317cb25afa118
AidenJ-jaco0160 <aidenwjacobs@hotmail.com>
2020-08-23 23:36:55 +0930

    Implemented Simple Saving/Loading
    Implemented a very basic saving and loading system, can be streamlined after some group discussion

---
+++ ZeroHeroes/Assets/User Test Folders/Aiden (jaco0160)/GameSaveData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class will contain the game's save data
+//When the game is saved, an instance of this class will be written to the user's device
+//When the game is loaded, an instance of this class will will be leaded inot ram to update game values
+[System.Serializable]
+public class GameSaveData{
+
+    //Add any data to be saved here
+
+    //Test data
+    public int[] testData1 = new int[5];
+    public int[] testData2 = new int[5];
+
+
+    //Not currently used
+    void Start(){}
+
+    // Any data being saved that should be updated every frame should be added here
+    public void updateRegularSaveData(){
+
+        /*e.g
+         * 
+         * this.playerPosition = GameManager.playerPosition;
+         * 
+         */
+        
+        //Test data
+        int randomIndex = Random.Range(0, testData1.Length);
+        int randomValue = Random.Range(0, 500);
+        testData1[randomIndex] = randomValue;
+    }
+
+    // Any data being saved irreguarly (at checkpoints, etc.) Should be updated here
+    public void UpdateCheckpoint(){
+
+        /*e.g
+         * 
+         * this.playerScore = GameManager.playerScore;
+         * 
+         */
+
+        //Test data
+        int randomIndex = Random.Range(0, testData2.Length);
+        int randomValue = Random.Range(0, 500);
+        testData2[randomIndex] = randomValue;
+    }
+
+    //This is where we update the in-game variables to the values loaded by the manager
+    public void restoreSavedData(){
+
+        /*e.g
+         * 
+         * GameManager.playerScore = this.playerScore;
+         * 
+         */
+    }
+}
---
+++ ZeroHeroes/Assets/User Test Folders/Aiden (jaco0160)/SaveLoadManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+//This is a static manager class to interact with the Game's saving system
+//This should be attached to the game manager
+public class SaveLoadManager : MonoBehaviour
+{
+    private static GameSaveData currentSaveData = new GameSaveData();
+
+    //This is the name of the file where the data will be stored to
+    private static string defaultSaveFileName;
+
+    // Start is called before the first frame update
+    void Start(){
+
+        //This sets the save location of the game's data to an external folder accessible on Windows and Android
+        defaultSaveFileName = Application.persistentDataPath + "/save.dat";
+
+        //Test Saving
+        for (int i = 0; i < 10; i++){
+            updateCheckpointData();
+        }
+        saveData();
+        loadData();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        currentSaveData.updateRegularSaveData();
+    }
+
+    public static void updateCheckpointData(){
+        currentSaveData.UpdateCheckpoint();
+    }
+
+    public static void saveData(){saveData(defaultSaveFileName);}
+
+    public static void saveData(string fileName){
+        //Save currentSaveData to a file
+        Debug.Log("Saving Game Data");
+        string destination = fileName;
+        FileStream file;
+
+        if (File.Exists(fileName)) file = File.OpenWrite(fileName);
+        else file = File.Create(fileName);
+        
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, currentSaveData);
+        file.Close();
+    }
+
+    public static void loadData() { loadData(defaultSaveFileName); }
+
+    public static void loadData(string fileName){
+        //Load currentSaveData from a file
+        Debug.Log("Loading Game Data");
+        FileStream file;
+
+        if (File.Exists(fileName)) file = File.OpenRead(fileName);
+        else
+        {
+            Debug.LogError("File not found");
+            return;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        currentSaveData = (GameSaveData)bf.Deserialize(file);
+        file.Close();
+
+        currentSaveData.restoreSavedData();
+    }
+}
