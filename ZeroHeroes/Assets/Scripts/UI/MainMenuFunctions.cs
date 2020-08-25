1b65170c50de6da977ef1ce762de8229764ebbfa
Planman-Dan <70016440+Planman-Dan@users.noreply.github.com>
2020-08-25 17:49:14 +0930

    Added Main Menu Functionality

---
+++ ZeroHeroes/Assets/Scripts/UI/MainMenuFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Plot");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
