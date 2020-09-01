df01c1f324d308cb5144c3b5d1ef7fd6a4ca3bad
Brayden Griffiths <brayden.jay.griffiths@gmail.com>
2020-09-01 14:23:58 +0930

    Some more building aspects
    c and v to switch between items

---
+++ ZeroHeroes/Assets/User Test Folders/Brayden (GRIF0282)/Script Tests/TestScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestScript : MonoBehaviour
+{
+
+    Tilemap tilemp; void Start() { tilemp = GameObject.Find("Foreground").GetComponent<Tilemap>(); }
+    public TileBase[] currentTile;
+
+    public int selectNum = 0;
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            selectNum++;
+            if(selectNum >= 3)
+            {
+                selectNum = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            selectNum--;
+            if(selectNum <= 0)
+            {
+                selectNum = 2;
+            }
+        }
+            /*if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                selectNum = 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                selectNum = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                selectNum = 2;
+            }*/
+        
+
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        point.z = Camera.main.nearClipPlane;
+        if (Input.GetMouseButtonDown(0)) {
+            //Camera.main.transform.position = (point);
+            Vector3Int selectedTile = tilemp.WorldToCell(point);
+            tilemp.SetTile(selectedTile, currentTile[selectNum]);
+        }
+    }
+
+    
+}
