d11a8c21a42e41678c2d9430e0fcee733d88dcc0
Brayden Griffiths <brayden.jay.griffiths@gmail.com>
2020-10-12 16:35:03 +1030

    Build system working with build botton to toggle
    Build system toggles on and off
    currently switches between items with c and v but will work with inventory when the inventory is ready to be used

--- ZeroHeroes/Assets/User Test Folders/Brayden (GRIF0282)/Script Tests/TestScript.cs
+++ ZeroHeroes/Assets/User Test Folders/Brayden (GRIF0282)/Script Tests/TestScript.cs
@@ -6,49 +6,123 @@ using UnityEngine.Tilemaps;
 public class TestScript : MonoBehaviour
 {
 
-    Tilemap tilemp; void Start() { tilemp = GameObject.Find("Foreground").GetComponent<Tilemap>(); }
+    Tilemap tilemp;
+    void Start() {
+        tilemp = GameObject.Find("Foreground").GetComponent<Tilemap>();
+    }
     public TileBase[] currentTile;
 
     public int selectNum = 0;
 
+    public TileBase sand_1;
+    public TileBase rock_1;
+    public TileBase reed_21;
+
+    public TileBase collision;
+
+    public bool turnedOn = true;
+
+    public string itemID;
+
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.C))
+
+        if(turnedOn == true)
         {
-            selectNum++;
-            if(selectNum >= 3)
+            if (Input.GetKeyDown(KeyCode.C))
             {
-                selectNum = 0;
+                selectNum++;
+                if(selectNum >= 3)
+                {
+                    selectNum = 0;
+                }
             }
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            selectNum--;
-            if(selectNum <= 0)
+            if (Input.GetKeyDown(KeyCode.V))
             {
-                selectNum = 2;
+                selectNum--;
+                if(selectNum <= 0)
+                {
+                    selectNum = 2;
+                }
             }
-        }
-            /*if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                selectNum = 1;
-            }
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                selectNum = 0;
-            }
-            if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                selectNum = 2;
-            }*/
+                /*if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    selectNum = 1;
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    selectNum = 0;
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    selectNum = 2;
+                }*/
         
 
-        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        point.z = Camera.main.nearClipPlane;
-        if (Input.GetMouseButtonDown(0)) {
-            //Camera.main.transform.position = (point);
-            Vector3Int selectedTile = tilemp.WorldToCell(point);
-            tilemp.SetTile(selectedTile, currentTile[selectNum]);
+            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            point.z = Camera.main.nearClipPlane;
+            if (Input.GetMouseButtonDown(0)) {
+                if(currentTile[selectNum] == sand_1)
+                {
+                    Debug.Log("move to foreground");
+                    tilemp = GameObject.Find("Foreground").GetComponent<Tilemap>();
+                    Vector3Int selectedTile = tilemp.WorldToCell(point);
+                    tilemp.SetTile(selectedTile, currentTile[selectNum]);
+                }
+                else if (currentTile[selectNum] == rock_1)
+                {
+                    Debug.Log("move to foreground 2");
+                    tilemp = GameObject.Find("Foreground 2").GetComponent<Tilemap>();
+                    Vector3Int selectedTile = tilemp.WorldToCell(point);
+                    tilemp.SetTile(selectedTile, currentTile[selectNum]);
+                    tilemp = GameObject.Find("Collision").GetComponent<Tilemap>();
+                    tilemp.SetTile(selectedTile, collision);
+                }
+                else if (currentTile[selectNum] == reed_21)
+                {
+                    Debug.Log("move to foreground 2");
+                    tilemp = GameObject.Find("Foreground 2").GetComponent<Tilemap>();
+                    Vector3Int selectedTile = tilemp.WorldToCell(point);
+                    tilemp.SetTile(selectedTile, currentTile[selectNum]);
+                }
+
+
+                //Camera.main.transform.position = (point);
+                //Vector3Int selectedTile = tilemp.WorldToCell(point);
+                //tilemp.SetTile(selectedTile, currentTile[selectNum]);
+            }
+        }
+        
+    }
+
+
+    void SelectedItem()
+    {
+        //itemID = GameObject.Find("TileBase").GetComponent<ID>();
+
+        if(itemID == "")
+        {
+            //selectNum = ;
+        }
+        if (itemID == "")
+        {
+            //selectNum = ;
+        }
+        if (itemID == "")
+        {
+            //selectNum = ;
+        }
+        if (itemID == "")
+        {
+            //selectNum = ;
+        }
+        if (itemID == "")
+        {
+            //selectNum = ;
+        }
+        if (itemID == "")
+        {
+            //selectNum = ;
         }
     }
 
