940581c9ae4202bf626b93eed1c971ffa38dc3e9
Brayden Griffiths <brayden.jay.griffiths@gmail.com>
2020-10-19 14:53:09 +1030

    More of older code concepts (Different Names)

---
+++ ZeroHeroes/Assets/User Test Folders/BuildingTestFolder/Scripts/Objects.cs
@@ -0,0 +1,60 @@
+﻿/* Object.cs
+* 
+* Author:  Nicholas Ruotsalainen RUOT0003
+* Created: 17/08/2020
+* 
+* Last Edited By: Nicholas Ruotsalainen RUOT0003
+* Last Updated:   18/08/2020
+*/
+
+using Assets.Scripts.world;
+using UnityEngine;
+
+namespace Assets.Scripts.world
+{
+    public class Objects : ObjectBase
+    {
+        public Object(string _id, string _definitionId, Position _position, ObjectData objectData) : base(_id, _definitionId, _position, objectData)
+        {
+            //intentionally left empty
+        }
+
+        public override void Destroy()
+        {
+            GameController.Instance.World.Remove(this);
+
+            if (GameObject != null)
+            {
+                UnityEngine.GameObject.Destroy(GameObject);
+            }
+        }
+
+        public override GameObject Spawn()
+        {
+            gameObject = GameObject.Instantiate(GameController.Instance.objectPrefabTemplate);
+
+            //set sprite
+            gameObject.GetComponent<SpriteRenderer>().sprite = ObjectData.sprite; //Definition.Sprite;
+            gameObject.GetComponent<SpriteRenderer>().color = ObjectData.color;
+
+            //add animation component if animatable
+            if (ObjectData.IsAnimatable)
+            {
+                gameObject.AddComponent<SpriteAnimator>().Initialize(ObjectData.sprites);//Definition.Sprites);
+            }
+
+            //set other data
+            gameObject.transform.position = position.ToVector();
+            gameObject.name = id;
+            gameObject.transform.SetParent(GameController.Instance.ObjectContainer, false);
+
+            return gameObject;
+        }
+
+        public override void OnInteraction()
+        {
+            objData.OnInteraction();
+            Destroy();
+        }
+    }
+}
